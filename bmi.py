8facdc9d5635070d66856aae1adcc57cd093b8af
Luke <khantsi.23@ichat.sp.edu.sg>
2024-04-29 14:37:21 +0800

    Updated bmi.py

--- bmi.py
+++ bmi.py
@@ -7,9 +7,12 @@ def calculate_bmi(height,weight):
 def is_healthy(bmivalue):
     if(bmivalue<18.5):
         print("UnderWeight")
+        return -1
     elif(bmivalue>25.0):
-        print("OverWeight") 
+        print("OverWeight")
+        return 1 
     else:
         print("Normal Weight")
+        return  0
 
 is_healthy(calculate_bmi(weight=10, height=1.73))
